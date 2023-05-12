52755112146cea924d0716f4d498cb2c7470f473
cs <clensesanthosh2017@gmail.com>
2023-05-12 18:56:46 +0530

    Updated something

--- Kennapi bot.py
+++ Kennapi bot.py
@@ -3,8 +3,12 @@ import discord
 import spotipy
 import spotipy.util as util
 from dotenv import load_dotenv
+import logging
 
 load_dotenv()
+
+# Set up logging
+logging.basicConfig(filename='discord_spotify.log', level=logging.ERROR)
 
 # Get environment variables
 TOKEN = os.getenv('DISCORD_BOT_TOKEN')
@@ -19,44 +23,61 @@ scope = 'user-read-playback-state,user-modify-playback-state,user-read-private'
 token = util.prompt_for_user_token(SPOTIPY_USERNAME, scope, SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI)
 sp = spotipy.Spotify(auth=token)
 
+voice_client = None
 
 @client.event
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
 
-
 @client.event
 async def on_message(message):
+    global voice_client
+    
     if message.author == client.user:
         return
 
     if message.content.startswith('!play'):
         try:
+            # Get the voice channel that the user is in
+            channel = message.author.voice.channel
+            if voice_client and voice_client.channel != channel:
+                await voice_client.disconnect()
+                voice_client = None
+            if not voice_client:
+                # Join the voice channel
+                voice_client = await channel.connect()
+                
+            # Get the query and search for the track
             query = message.content.split(' ', 1)[1]
             results = sp.search(q=query, type='track', limit=1)
             if results['tracks']['items']:
                 track_uri = results['tracks']['items'][0]['uri']
-                sp.start_playback(uris=[track_uri])
+                # Play the track in the voice channel
+                voice_client.play(discord.FFmpegPCMAudio(executable='ffmpeg', source=sp.track(track_uri)['preview_url']))
                 await message.channel.send(f'Now playing: {results["tracks"]["items"][0]["name"]}')
         except Exception as e:
-            print(e)
-            await message.channel.send('Error: Failed to play song')
+            error_message = f'Error: Failed to play song: {e}'
+            logging.error(error_message)
+            await message.channel.send(error_message)
 
     elif message.content == '!stop':
         try:
-            sp.pause_playback()
+            await voice_client.disconnect()
+            voice_client = None
             await message.channel.send('Playback stopped')
         except Exception as e:
-            print(e)
-            await message.channel.send('Error: Failed to stop playback')
+            error_message = f'Error: Failed to stop playback: {e}'
+            logging.error(error_message)
+            await message.channel.send(error_message)
 
     elif message.content == '!skip':
         try:
             sp.next_track()
             await message.channel.send('Skipped to the next song')
         except Exception as e:
-            print(e)
-            await message.channel.send('Error: Failed to skip to the next song')
+            error_message = f'Error: Failed to skip to the next song: {e}'
+            logging.error(error_message)
+            await message.channel.send(error_message)
 
     elif message.content.startswith('!playlist'):
         try:
@@ -64,7 +85,8 @@ async def on_message(message):
             sp.start_playback(context_uri=playlist_uri)
             await message.channel.send('Now playing playlist')
         except Exception as e:
-            print(e)
-            await message.channel.send('Error: Failed to play playlist')
+            error_message = f'Error: Failed to play playlist: {e}'
+            logging.error(error_message)
+            await message.channel.send(error_message)
 
 client.run(TOKEN)
